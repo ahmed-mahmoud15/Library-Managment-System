9ada16ae1b5e71bbd09347ffcfe9b8babb0d490d
Ahmed Mahmoud <68530005+ahmed-mahmoud15@users.noreply.github.com>
2022-12-21 16:23:26 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned int N;
+
+    cin >> N;
+    for (int i = 1; i <= 12 ; i++)
+        cout << N << " * " << i << " = " << N * i << endl;
+    return 0;
+}
